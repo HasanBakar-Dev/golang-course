a26ee23f5f1f19355a10985affdd5f58f3e993bc
HasanBakar-Dev <277088193+HasanBakar-Dev@users.noreply.github.com>
2026-05-09 08:41:27 +0600

    golang chapter Two :tada: :book:

---
+++ 2_simple_values/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	// int
+	fmt.Println(44)
+	// floats
+	fmt.Println(7.0 / 3.0)
+	// string
+	fmt.Println("hey there golang")
+	// boolean
+	fmt.Println(true)
+	fmt.Println(false)
+}
